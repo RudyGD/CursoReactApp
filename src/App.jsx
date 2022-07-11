701e0504ce47b774a304a070c7218ab290e952ab
RudyGD <gonzalezdiesterudy@gmail.com>
2022-07-11 12:39:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import { Heading, Image } from '@chakra-ui/react';
+import { LaunchItem } from './components/LaunchItem';
+import * as API from './services/launches';
+import logo from './assets/logo-spacex.png';
+
+
+export function App() {  
+  const [launches, setLaunches] = useState([]);
+
+  //recibe dos argumentos, el segundo es un array de dependencias
+  useEffect(() => {
+    API.getAllLaunches().then(setLaunches);
+  }, []);
+
+  return (
+    <>
+    <Image m={4} src={logo}  width={300}/>
+    <Heading as="h1" size="lg" m={4}>
+      SpaceX Launches
+    </Heading>
+      <section>        
+        {launches.map((launch)=>(          
+          <LaunchItem key={launch.flight_number} {...launch}/>
+        ))}
+      </section>
+    </>
+  );
+}
+
+
+
+
